13881c0e00918d8b6960fbe5d9705488d8c4cc99
Joaquin Akerman <55959401+JoaquinAkerman@users.noreply.github.com>
2023-06-27 14:59:27 -0300

    remove warning for online demo

---
+++ client/src/utils/demoWarning.js
@@ -0,0 +1,15 @@
+const demoWarning = (
+  <div className="warning">
+    <p>
+      Not available on demo. Full version{" "}
+      <a
+        className="demo-link"
+        href="https://github.com/JoaquinAkerman/crud-clubs-2"
+      >
+        DOWNLOAD AND INSTALL
+      </a>
+    </p>
+  </div>
+);
+
+export { demoWarning };
